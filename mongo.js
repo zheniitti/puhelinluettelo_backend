7d5f281ab412def4013f323490def97846c77c50
Zheniitti <liao.zhenyu.zl@gmail.com>
2021-10-09 21:22:29 +0300

    valmis Tehtävä 3.12

--- mongo.js
+++ mongo.js
@@ -4,29 +4,45 @@ if (process.argv.length < 3) {
     console.log('give password as argument')
     process.exit(1)
 }
+else {
+    const password = process.argv[2]
+    const name = process.argv[3]
+    const number = process.argv[4]
 
-const password = process.argv[2]
+    const url =
+        `mongodb+srv://fullstack21:${password}@cluster0.sfml8.mongodb.net/phonebook?retryWrites=true&w=majority`
+    mongoose.connect(url)
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+    })
 
-const url =
-    `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+    const Person = mongoose.model('Person', personSchema)
 
-mongoose.connect(url)
+    if (process.argv.length === 3) {
+        console.log('phonebook:')
+        Person.find({}).then(result => {
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+    } else {
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-})
+        const person = new Person({
+            name: name,
+            number: number,
+        })
 
-const Note = mongoose.model('Note', noteSchema)
+        person.save().then(response => {
+            console.log(`added ${name} number ${number} to phonebook`)
+            mongoose.connection.close()
+        })
+    }
+}
 
-const note = new Note({
-    content: 'HTML is Easy',
-    date: new Date(),
-    important: true,
-})
 
-note.save().then(response => {
-    console.log('note saved!')
-    mongoose.connection.close()
-})
+
+
+
+
